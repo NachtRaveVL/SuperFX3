117224486073e1acc4212ab455ab72b2dd6dfe81
NachtRave <NachtRaveVL@users.noreply.github.com>
2026-08-18 21:02:19 -0600

    linter fix

---
+++ src/SuperFX3.h
@@ -0,0 +1,15 @@
+/*
+ * NR-RetroWorks SuperFX3 Firmware
+ * Copyright (C) 2026 NR-RetroWorks
+ *
+ * Arduino library compatibility header.
+ *
+ * SuperFX3 firmware is built with the Raspberry Pi Pico SDK. This umbrella
+ * header exposes the platform-independent Super FX / GSU core API while also
+ * providing the conventional src/SuperFX3.h entry point expected by Arduino
+ * library tooling.
+ */
+
+#pragma once
+
+#include "fx/fx_core.h"
